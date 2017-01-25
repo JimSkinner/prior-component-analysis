dcdca4edf6f05fda21bc345c7bfa5710b1c7ef2b
JimSkinner <jskinner.mail@gmail.com>
2017-01-25 16:03:21 +0000

    Useful code to compute principal angled between subspaces (for comparing learned Ws)

---
+++ tests/denoise/principal-angles.R
@@ -0,0 +1,9 @@
+principalAngles <- function(A, B) {
+  Qa = qr.Q(qr(A))
+  Qb = qr.Q(qr(B))
+
+  C = svd(crossprod(Qa, Qb))$d
+  C = vapply(C, function(x) min(x, 1), numeric(1))
+  angles = acos(C)
+  return(angles)
+}
