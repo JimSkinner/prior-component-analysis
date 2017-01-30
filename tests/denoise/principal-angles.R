7b76b4dd3d2108aca4b1211b216412b885f7accc
JimSkinner <jskinner.mail@gmail.com>
2017-01-30 11:13:48 +0000

    Moved (to be used in more tests)

--- tests/denoise/principal-angles.R
+++ tests/denoise/principal-angles.R
@@ -1,7 +1,7 @@
 principalAngles <- function(A, B) {
+  stopifnot(all(dim(A) == dim(B)))
   Qa = qr.Q(qr(A))
   Qb = qr.Q(qr(B))
-
   C = svd(crossprod(Qa, Qb))$d
   C = vapply(C, function(x) min(x, 1), numeric(1))
   angles = acos(C)
